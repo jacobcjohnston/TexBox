7cf1fa2c27cf31c007510526e72e28f6f3c9457d
jacobcjohnston <jakejohnston102@gmail.com>
2024-11-07 21:10:26 -0500

    Reworking the system to involve React Contexts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import {useCallback, useEffect, useRef, useState } from 'react';
+import {useCallback, useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 import Draggable from "react-draggable";
 
 
+
+
+
 class State {
-  constructor(action, result, position=[0, 0]) {
+  constructor(action, result, parent=null, position=[0, 0]) {
       this.action = action;
       this.result = result;
+
+      this.parent = parent;
       this.children = [];
-      this.position = [0, 0]
+      this.position = position;
 
       const updatePos = (data) => {
         this.position = data;
@@ -20,12 +25,13 @@ class State {
         result = {this.result}
         updatePosition = {updatePos}
         position={this.position}
+        parentPos={parent ? parent.position : parent}
         key={this.action}
       />
   }
 
   addChild(action, result) {
-      const child = new State(action, result)
+      const child = new State(action, result, this)
       this.children.push(child)
   }
   
@@ -36,7 +42,6 @@ class State {
       }
     }
     return -1;
-    
   }
 
 }
@@ -45,18 +50,22 @@ class State {
 
 
 
-function Node({action, result, updatePosition, position = [0, 0]}) {
+function Node({action, result, updatePosition, parentPos, position = [0, 0]}) {
 
-  const pos = useRef(position);
+  const [pos, setPos] = useState(position);
 
   const trackDrag = (e, ui) => {
-    const [x, y] = pos.current;
-    pos.current = (
+    const [x, y] = pos;
+    setPos(
       [x + ui.deltaX,
         y + ui.deltaY,]
     );
-    updatePosition(pos.current)
+    updatePosition(pos)
   }
+
+  //const [parentPos, setParentPos] = useState(parent.position)
+
+
 
   return (
     <div>
@@ -64,9 +73,14 @@ function Node({action, result, updatePosition, position = [0, 0]}) {
         defaultPosition={{x:position[0], y:position[1]}}
         onDrag={trackDrag}
       >
-        <div className='Node'>
-          <h1>{action}</h1>
-          <p>{result}</p>
+        <div>
+          <div className='Node'>
+            <h1 className="Action">{action}</h1>
+            <textarea></textarea>
+          </div>
+          {
+            (parentPos) ? (<Line startPos={[parentPos[0] - pos[0], parentPos[1] - pos[1]]} endPos = {[100, 5]}/>) : (<div></div>)
+          }
         </div>
       </Draggable>
     </div>
@@ -75,9 +89,34 @@ function Node({action, result, updatePosition, position = [0, 0]}) {
 
 
 function Line ({startPos, endPos}) {
-  return (
-    <div className="Line">
+  const width = Math.abs(startPos[0] - endPos[0]);
+  const height = Math.abs(startPos[1] - endPos[1]);
 
+
+
+  return (
+    <div>
+      <div className="Line Horizontal" style = {
+        {
+          width:width+6,
+          left:Math.min(startPos[0], endPos[0]),
+          top:height/2 + startPos[1],
+        }
+      }></div>
+      <div className="Line Vertical" style = {
+        {
+          height:height/2+6,
+          left:startPos[0],
+          top:startPos[1],
+        }
+      }></div>
+      <div className="Line Vertical" style = {
+        {
+          height:height/2,
+          left:endPos[0],
+          top:height/2 + startPos[1],
+        }
+      }></div>
     </div>
   );
 }
@@ -95,11 +134,9 @@ function parseStory(story) {
 
 
 
-const story = new State("Start", "You are here")
+const story = new State("Start", "You are here", null, [0, -300])
 story.addChild("Left", "You are left")
 story.addChild("Right", "You are Right")
-
-story.getChild("Right").addChild("Forward", "You are Right and forward")
 
 console.log(story)
 console.log(parseStory(story))
@@ -128,10 +165,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='Sidebar'>
-          <h1>Settings</h1>
-        </div>
-        {data}
+
+          {data}
+      
       </header>
     </div>
   );
